5df099194cdfbc751190c93a20d4d11d157a957d
NotReallySomeone <xcvrty137869585ggjhdk@gmail.com>
2022-11-03 03:09:01 -0300

    Pages that don't work

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// Pages
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Dashboard'
+import NoPage from './pages/Nopage'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}></Route>
+      <Route path="Dashboard" element={<Dashboard />}></Route>
+      <Route path="*" element={<NoPage />}></Route>
+      <Route path="Login" element={<Login />}></Route>
     </Routes>
   </BrowserRouter>
 )
